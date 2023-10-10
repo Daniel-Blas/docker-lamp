a099d490a21984867222b832e77fcf39ee89ddce
Daniel Blas Andrade <a22carlosba@iessanclemente.net>
2023-10-10 13:43:54 +0200

    tarea 6

--- www/ud02/01introduccion/tarea6.php
+++ www/ud02/01introduccion/tarea6.php
@@ -4,4 +4,10 @@
 
 //Crea un programa en PHP que guarde en una variable tu nombre y lo muestre en negrita formando el siguiente mensaje: Bienvenido tunombre
 
+echo "Hola, Mundo!<br>";
+echo "<i>Hola, Mundo!</i><br>";
+
+$nombre = "Dani";
+echo "Bienvenido <b>$nombre</b><br>";
+
 ?>
