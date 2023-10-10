0ffe4db3de3ae87435d070b801621beb4e15ecd7
Daniel Blas Andrade <a22carlosba@iessanclemente.net>
2023-10-10 13:13:36 +0200

    tarea 4

--- www/ud02/01introduccion/tarea4.php
+++ www/ud02/01introduccion/tarea4.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 
 /*Haz una página que ejecute la función phpinfo() y que muestre las principales variables de servidor mencionadas en teoría.
 */
-
+phpinfo();
 
 ?>
+    
+</body>
+</html>
