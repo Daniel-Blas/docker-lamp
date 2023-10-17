c4ea8f1ff9bf582a87f39725177356d8c7db7fe4
Daniel Blas Andrade <a22carlosba@iessanclemente.net>
2023-10-17 11:25:36 +0200

    arrays, tarea 2

--- www/ud02/02arrays/tarea2.php
+++ www/ud02/02arrays/tarea2.php
@@ -27,5 +27,24 @@ Necesitamos almacenar la siguiente información en un array multidimensional:
 Almacena e imprime la información. 
 */
 
+$personas = array(
+    "John" => array("email" => "john@demo.com", "website" => "www.john.com", "age" => 22, "password" => "pass"),
+    "Anna" => array("email" => "anna@demo.com", "website" => "www.anna.com", "age" => 24, "password" => "pass"),
+    "Peter" => array("email" => "peter@mail.com", "website" => "www.peter.com", "age" => 42, "password" => "pass"),
+    "Max" => array("email" => "max@mail.com", "website" => "www.max.com", "age" => 33, "password" => "pass")
+);
+
+echo "<ul>";
+foreach ($personas as $p => $info){
+    echo "<li>", $p;
+    echo "<ul>";
+    echo "<li>", "email: {$info['email']}", "</li>";
+    echo "<li>", "website: {$info['website']}", "</li>";
+    echo "<li>", "age: {$info['age']}", "</li>";
+    echo "<li>", "password: {$info['password']}", "</li>";
+    echo "</ul>";
+    echo "</li>";
+}
+echo "</ul>";
 
 ?>
