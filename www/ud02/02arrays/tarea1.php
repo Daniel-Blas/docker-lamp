973394a3077866f30013287bd94b818cad6439b8
Daniel Blas Andrade <a22carlosba@iessanclemente.net>
2023-10-17 10:51:38 +0200

    arrays, tarea 1

--- www/ud02/02arrays/tarea1.php
+++ www/ud02/02arrays/tarea1.php
@@ -2,9 +2,23 @@
 
 //1. Almacena en un array los 10 primeros números pares. Imprímelos cada uno en una línea utilizando los valores contenidos en el array.
 
+for ($i=0; $i<10; $i++){
+    $pares[$i] = $i * 2;
+}
+foreach ($pares as $n){
+    echo $n, "<br />";
+}
+
+echo "<br/>";
+
 /* 2. Imprime los valores del array asociativo siguiente usando un foreach:*/
 $v[1]=90;
 $v[10] = 200;
 $v['hola']=43;
 $v[9]='e';
+
+foreach($v as $n){
+    echo $n, "<br/>";
+}
+
 ?>
