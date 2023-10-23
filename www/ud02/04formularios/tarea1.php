d2eb46090c2ceade3ad990714954123a6f741095
Daniel Blas Andrade <a22carlosba@iessanclemente.net>
2023-10-23 21:17:51 +0200

    formularios, tarea 1

--- www/ud02/04formularios/tarea1.php
+++ www/ud02/04formularios/tarea1.php
@@ -6,6 +6,15 @@
     <body>
         <div>
             <!-- Aquí va el formulario-->
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre">
+            <br/>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos">
+            <br/>
+            <input type="submit">
+            </form>
         </div>
 
             <div>
@@ -24,6 +33,29 @@
                     */
                     
                     //Aquí el código php que muestra la información solicitada.
+                    echo "<br/>
+                        Nombre: '", $_POST['nombre'], "'";
+                    echo "<br/>
+                        Apellidos: '", $_POST['apellidos'], "'";
+                    echo "<br/>
+                        Nombre y apellidos: '", $_POST['nombre'], " ", $_POST['apellidos'], "'";
+                    echo "<br/>
+                        Su nombre tiene caracteres '", strlen($_POST['nombre']), "'";
+                    echo "<br/>
+                        Los 3 primeros caracteres de tu nombre son: '",substr($_POST['nombre'], 0, 3), "'";
+                    echo "<br/>
+                        La letra A fue encontrada en sus apellidos en la posición: '", (strpos(strtoupper($_POST['apellidos']), 'A') + 1), "'";
+                    echo "<br/>
+                        Tu nombre en mayúsculas es: '", strtoupper($_POST['nombre']), "'";
+                    echo "<br/>
+                        Sus apellidos en minúsculas son: '", strtolower($_POST['apellidos']), "'";
+                    echo "<br/>
+                        Su nombre y apellidos en mayúsculas: '", strtoupper($_POST['nombre']), " ", strtoupper($_POST['apellidos']), "'";
+                    echo "<br/>
+                        Tu nombre escrito al revés es: '", strrev($_POST['nombre']), "'";
+
+
+                    
                 ?>
         </div>
     </body>
