450e1f40150d081ebeae19dc0ab37d18330df526
Daniel-Blas <a22carlosba@iessanclemente.net>
2024-03-27 12:58:41 +0100

    Tabla hoteles

--- www/ud06/index.php
+++ www/ud06/index.php
@@ -76,4 +76,70 @@ Flight::route('PUT /clientes', function(){
 });
 
 
+// Tabla hoteles
+
+// Información de todos los hoteles
+Flight::route('GET /hoteles', function(){
+    $sentencia = Flight::db()->prepare("SELECT * from hoteles");
+    $sentencia->execute();
+    $datos=$sentencia->fetchAll();
+    Flight::json($datos);
+});
+
+// Obtener la información de un hotel en concreto
+Flight::route('GET /hoteles/@id', function (int $id) {
+    $sentencia = Flight::db()->prepare("SELECT * from hoteles WHERE id = ?");
+    $sentencia->bindParam(1, $id);
+    $sentencia->execute();
+    $datos=$sentencia->fetchAll();
+    Flight::json($datos);
+});
+
+// Insertar un hotel
+Flight::route('POST /hoteles', function(){
+    $hotel = Flight::request()->data->hotel;
+    $direccion = Flight::request()->data->direccion;
+    $email = Flight::request()->data->email;
+    $telefono = Flight::request()->data->telefono;
+
+    $sql = "INSERT INTO hoteles(hotel, direccion, telefono, email) VALUES(?, ?, ?, ?)";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1, $hotel);
+    $sentencia->bindParam(2, $direccion);
+    $sentencia->bindParam(3, $telefono);
+    $sentencia->bindParam(4, $email);
+
+    $sentencia->execute();
+    Flight::jsonp("Hotel agregado con éxito");
+});
+
+// Borrar un hotel dado un id
+Flight::route('DELETE /hoteles', function(){
+    $id = Flight::request()->data->id;
+    $sql = "DELETE FROM hoteles WHERE id = ?";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1, $id);
+    $sentencia->execute();
+    Flight::jsonp("El hotel con id $id ha sido eliminado");
+});
+
+// Modificar un hotel su dirección, email y teléfono
+Flight::route('PUT /hoteles', function(){
+    $id = Flight::request()->data->id;
+    $direccion = Flight::request()->data->direccion;
+    $email = Flight::request()->data->email;
+    $telefono = Flight::request()->data->telefono;
+
+    $sql = "UPDATE hoteles SET direccion = ?, email = ?, telefono = ? WHERE id = ?";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1, $direccion);
+    $sentencia->bindParam(2, $email);
+    $sentencia->bindParam(3, $telefono);
+    $sentencia->bindParam(4, $id);
+
+    $sentencia->execute();
+    Flight::jsonp("Hotel actualizado con éxito");
+});
+
+
 Flight::start();
