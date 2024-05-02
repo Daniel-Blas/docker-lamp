5f4febe02addc762b4c1fb1213e96c5add231b30
Daniel-Blas <a22carlosba@iessanclemente.net>
2024-05-02 13:26:37 +0200

    Terminado

--- www/ud07/symfony_projects/src/Controller/bibliotecaController.php
+++ www/ud07/symfony_projects/src/Controller/bibliotecaController.php
@@ -1,25 +1,40 @@
 <?php
+
 namespace App\Controller;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class bibliotecaController{
+
+class bibliotecaController extends AbstractController
+{
 
     #[Route('/')]
-    public function homepage(){
-        return new Response("homepage");
+    public function homepage()
+    {
+        return $this->render('biblioteca/homepage.html.twig', ['title' => 'Biblioteca']);
     }
 
     #[Route('/libros')]
-    public function libros(){
-        return new Response("Listado de libros");
+    public function libros()
+    {
+        $libros = [
+            ['nombre' => "El capitán Alatriste", 'autor' => "Pérez-Reverte"],
+            ['nombre' => "Os dous de sempre", 'autor' => "Castelao"],
+            ['nombre' => "El halcón maltes", 'autor' => "Dashiel Hammett"],
+            ['nombre' => "Rescate en el tiempo", 'autor' => "Michael Crichton"],
+
+        ];
+        return $this->render('biblioteca/libros.html.twig', [
+            'title' => 'Libros',
+            'libros' => $libros
+        ]);
     }
 
     #[Route('/contacto')]
-    public function contacto(){
-        return new Response("Contacto");
+    public function contacto()
+    {
+        return $this->render('biblioteca/contacto.html.twig', ['title' => 'Contacto']);
     }
-
 }
-
-?>
