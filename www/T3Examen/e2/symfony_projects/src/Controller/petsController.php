7417483cee8026c661b64dc985598552b3965748
Nombre de alumno <nombre@alumno>
2024-05-27 21:56:07 +0200

    terminado

---
+++ www/T3Examen/e2/symfony_projects/src/Controller/petsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+// use Psr\Cache\CacheItemInterface;
+
+class petsController extends AbstractController {
+
+    #[Route('/pets')]
+    public function pets(HttpClientInterface $httpClient){
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/Daniel-Blas/docker-lamp/main/www/T3Examen/pets.json');
+        $pets = $response->toArray();
+
+
+        return $this->render('pets.html.twig', [
+            'title' => 'Pets',
+            'pets' => $pets
+        ]);
+    }
+
+
+    #[Route('/petscache')]
+    public function petscache(HttpClientInterface $httpClient, CacheInterface $cache){
+        $pets = $cache->get('pets', function(CacheItemInterface $cacheItem) use ($httpClient) {
+            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/Daniel-Blas/docker-lamp/main/www/T3Examen/pets.json');
+            $cacheItem->expiresAfter(10);
+            return $response->toArray();
+        });
+
+
+        return $this->render('pets.html.twig', [
+            'title' => 'Pets',
+            'pets' => $pets
+        ]);
+    }
+
+}
